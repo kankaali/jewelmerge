2c3f3a527573f6ca35d09635d233d04e3e591bf1
kankaali <contactus@kankaali.com>
2026-02-03 21:30:18 +0530

    Update game.js

--- game.js
+++ game.js
@@ -125,10 +125,6 @@ function merge(a, b) {
   )
 }
 
-ctx.fillStyle = "red"
-ctx.beginPath()
-ctx.arc(canvas.width / 2, canvas.height / 2, 20, 0, Math.PI * 2)
-ctx.fill()
 
 
 // ====== UTILS ======
